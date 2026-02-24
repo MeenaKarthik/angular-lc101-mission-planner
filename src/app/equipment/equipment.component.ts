9efb372ac0aea95df625f65df45af9cb1d9a6bc9
MeenaKarthik <meenafresh@gmail.com>
2026-02-24 10:24:39 -0600

    add, edit & remove equipment

--- src/app/equipment/equipment.component.ts
+++ src/app/equipment/equipment.component.ts
@@ -7,9 +7,35 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EquipmentComponent implements OnInit {
 
+  equipment: string[] = ["Habitat dome", "Drones", "Food containers", "Oxygen tanks"];
+  toolBeingEdited: string = null;
+
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(toolName: string){
+    const cleanName =  toolName.trim().toLowerCase();
+    if(!this.equipment.some(e => e.toLowerCase() === cleanName)){
+      this.equipment.push(toolName);
+    }
+  }
+
+  edit(toolName: string){
+     this.toolBeingEdited = toolName;
+  }
+
+  save(updatedName: string, tool: string){
+      const index = this.equipment.indexOf(tool);
+      if(index > -1){
+        this.equipment[index]=updatedName.trim();
+      }
+      this.toolBeingEdited = null;
+  }
+
+  remove(toolName: string){
+      let index = this.equipment.indexOf(toolName);
+      this.equipment.splice(index,1);
+  }
 }
