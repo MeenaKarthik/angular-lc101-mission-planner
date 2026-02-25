3c1a6ff3f49c1c0946bd1fd71d9231faa43b41c4
MeenaKarthik <meenafresh@gmail.com>
2026-02-25 11:26:31 -0600

    add, edit & remove experiments

--- src/app/experiments/experiments.component.ts
+++ src/app/experiments/experiments.component.ts
@@ -7,9 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ExperimentsComponent implements OnInit {
 
+  experiments: string[] = ["Mars soil sample", "Plant growth in habitat", "Human bone density"];
+  experimentBeingEdited: string = null;
   constructor() { }
 
   ngOnInit() {
   }
 
+  add(experimentName: string){
+      const cleanName = experimentName.trim().toLowerCase();
+      if(!this.experiments.some(e => e.toLowerCase() === cleanName)){
+        this.experiments.push(experimentName);
+      }
+  }
+
+  edit(experimentName: string){
+    this.experimentBeingEdited = experimentName;
+  }
+
+  save(updatedName: string, experiment: string){
+    const index = this.experiments.indexOf(experiment);
+    if(index > -1){
+      this.experiments[index] = updatedName.trim();
+    }
+    this.experimentBeingEdited = null;
+  }
+
+  remove(experimentName: string){
+      let index = this.experiments.indexOf(experimentName);
+      this.experiments.splice(index, 1);
+  }
 }
